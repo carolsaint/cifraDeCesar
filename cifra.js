8d39fdd3070bcecc8a7b0f427f9b552c6842f5c8
carolsaint <santocarolina98@gmail.com>
2022-04-05 19:26:28 -0300

    encoder fixed

--- cifra.js
+++ cifra.js
@@ -1,21 +1,19 @@
 function criptografar(){
-    var texto = document.querySelector('.textInput').value.toLowerCase();
+    var texto = document.getElementById('textInput').value.toLowerCase();
     var textoCriptografado = "";
-    var shift = document.querySelector('.shift').value;
+    var shift = parseInt(document.querySelector('.shift').value);
     
     console.log(texto);
     console.log(shift);
     
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    
-    console.log(textoCriptografado);
 
-for (var i = 0; i < texto.length; i++) {
+    for (var i = 0; i < texto.length; i++) {
     var letra = texto[i];
     if (letra === " ") {
       textoCriptografado += letra;
     } else {
-      var indice = letra.charCodeAt() - 97 + shift;
+      var indice = parseInt(letra.charCodeAt() - 97 + shift);
       if (indice > 25) {
           var help = parseInt(indice / 26);
         indice = indice - (help * 26);
@@ -24,29 +22,27 @@ for (var i = 0; i < texto.length; i++) {
     }
   }
   console.log(textoCriptografado);
-
   return textoCriptografado
 }
 
 
-var iniciar = document.querySelector("#submitBtn");
+var iniciar = document.querySelector('.submitBtn');
 var opt1 = document.querySelector('.cryptButton');
 var opt2 = document.querySelector('.decryptButton');
-var output = document.querySelector('.encoded-text')
+var output = document.querySelector('.textOutput')
 
+document.querySelector('#container').addEventListener('submit', function(event){
+    event.preventDefault();
+}) 
 
 iniciar.addEventListener('click', function () {
-    if(opt1.checked == true){
+    if(opt1.checked === true){
         var textoCriptografado = criptografar();
         output.value = textoCriptografado;
     }else if(opt2.checked == true){
     
     }
 });
-
-document.querySelector('#container').addEventListener('submit', function(event){
-    event.preventDefault();
-}) 
 
 
 
